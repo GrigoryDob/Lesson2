53d301396db330111c397217439e3cfb9cc8fae5
GrigoryDob <gregy217@gmail.com>
2024-12-04 18:11:00 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9**0.5*5)
+
+# 2nd program
+print(9.99>9.98 and 1000!=1000.1)
+
+# 3rd program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+
+# 4th program
+print(int(float('123.456')*10)%10)
+
+# 5th and 6th programs (from alternative homework file)
+print(1234//10%100 + 5678//10%100)
+
+# 6th program
+print(13.42//1 == 42.13*100%100 and 42.13//1 == 13.42*100%100)
